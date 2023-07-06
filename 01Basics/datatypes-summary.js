6fa2de63539b72d29301aa3e61be9ac114361e33
Aashish <65489287+aashish36@users.noreply.github.com>
2023-07-06 22:18:58 +0530

    Update datatypes-summary.js

--- 01Basics/datatypes-summary.js
+++ 01Basics/datatypes-summary.js
@@ -35,3 +35,14 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+/*
+Primitive dataType goes in stack memory; And it provides copy of that element.
+So when we change something (through assigned variable) it gets changed in copy of element  not in original.
+
+
+Opposite of this. Non primitive dataType get stored in HEAP memory. 
+And it gives reference value to assigned elements. 
+So when assigned variable makes changes it changes the both values(original and assigned one. 
+*/
