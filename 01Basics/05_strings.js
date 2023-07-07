0ee6d7e34ac46e5b281df0af018d6c68def0255e
Aashish <65489287+aashish36@users.noreply.github.com>
2023-07-07 02:36:55 +0000

    learned about string through browser console

--- 01Basics/05_strings.js
+++ 01Basics/05_strings.js
@@ -1,1 +1,23 @@
 //console.log("jai shree ram");
+const god="Shree Ram";
+const myName="Aashish"
+const myAge=21;
+console.log(`My Name is ${myName}. I age is ${myAge}. I love Prabus ${god}`)
+
+const newName=new String("Balaya-Hulk");
+console.log(newName.length);
+console.log(newName.charAt(2));
+console.log(newName.indexOf("a"));
+console.log(newName.substring(0,5));
+console.log(newName.slice(0,6));
+
+const newName1="    jai    shree   ram  ";
+console.log(newName1);
+console.log(newName1.trim());
+
+const newUrl="https://aashish%20bande.in/projects"
+console.log(newUrl.replace("%20","-"));
+console.log(newUrl.split("/"));
+console.log(newUrl.toLocaleLowerCase());
+console.log(newUrl.toLocaleUpperCase());
+console.log(newUrl.match("://"));
