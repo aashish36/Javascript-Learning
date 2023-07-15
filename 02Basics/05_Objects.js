b0e0978c770dbe973e6e13c059a5050c28339b74
Aashish <65489287+aashish36@users.noreply.github.com>
2023-07-15 12:01:39 +0000

    JSON

---
+++ 02Basics/05_Objects.js
@@ -0,0 +1,11 @@
+// object destructure
+const course={
+    name:"Etical hacking",
+    price:"12345",
+    teacher:"Aashish"
+}
+
+const {teacher:Instructur}=course
+
+//console.log(teacher);
+console.log(Instructur);
